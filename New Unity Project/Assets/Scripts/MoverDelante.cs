d65b19ecd156a917746078072fcc300f3c3eb4ff
10GoPro <diesgomen@gmail.com>
2015-04-12 14:17:40 -0500

    Comienzo
    Oe mire lo que llevo :v

---
+++ New Unity Project/Assets/Scripts/MoverDelante.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverDelante : MonoBehaviour {
+	public float Vel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
