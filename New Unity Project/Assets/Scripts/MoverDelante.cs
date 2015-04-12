0810244d32f809a697e42da85d9301e2d521efa1
10GoPro <diesgomen@gmail.com>
2015-04-12 16:18:58 -0500

    Ahora si todo

---
+++ New Unity Project/Assets/Scripts/MoverDelante.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverDelante : MonoBehaviour {
+	public float Vel;
+	private Animator animator;
+	// Use this for initialization
+	void Awake ()
+	{
+		animator = GetComponent <Animator>();
+	}
+	void Start () {
+		animator.SetFloat("VAdelante" , 0.02f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, Input.acceleration.y);
+
+		if( movement != Vector3.zero) {
+			GetComponent<Rigidbody>().AddForce(movement);
+		}
+	}
+}
